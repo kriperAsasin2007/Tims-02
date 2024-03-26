9331b506840152f812aaaba17cd877909ec6d70b
Yaroslav <ssaputskyy@gmail.com>
2024-03-26 08:57:04 +0200

    Added check and fix ni first try

--- src/distributionsCheks/NormalDistCheck.jsx
+++ src/distributionsCheks/NormalDistCheck.jsx
@@ -1,6 +1,32 @@
 import ShowTableContinious from "../utils/ShowTableContinious";
 
+const checkNi = (ni) => {
+    for (let i = 0; i < ni.length; i++) {
+        if(ni[i] < 5) {
+            return i;
+        }
+    }
+}
+
+
+
 const NormalDistCheck = ({a, sigma, intervals, ni}) => {
+    const fixNi = (ni, index) => {
+        
+        let tempH = intervals[index];
+        let tempN = ni[index];
+
+        intervals.splice(index, index);
+        ni.splice(index, index);
+        if(index !== 0) {
+            intervals[index-1][1] = tempH[1];
+            ni[index-1] += tempN;
+        }
+        else {
+            intervals[index+1][0] = tempH[0];
+            ni[index+1] += tempN;
+        }
+    }
     return ( 
         <div className='normal-dist-container'>
             <ShowTableContinious intervals={intervals} ni={ni} />
