1e2a9e7eabf7bd55a67ae56df070e2eff26f99f8
Yaroslav <ssaputskyy@gmail.com>
2024-03-26 11:42:23 +0200

    Added working refactoring of table without visible iterations

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import NormalDistCheck from './distributionsCheks/NormalDistCheck';
 const intervals = [[90,94], [94,98], [98,102], [102,106], [106,110]];
 const ni = [4,24,30,30,12];
 
+// const intervals = [[0,6], [6,12], [12,18], [18,24], [24,30], [30,36], [36,42]];
+// const ni = [115,51,18,9,4,2,1];
+
 function App() {
   return (
     <div className="App">
--- src/distributionsCheks/NormalDistCheck.jsx
+++ src/distributionsCheks/NormalDistCheck.jsx
@@ -1,35 +1,77 @@
+import { useState } from "react";
 import ShowTableContinious from "../utils/ShowTableContinious";
 
-const checkNi = (ni) => {
-    for (let i = 0; i < ni.length; i++) {
-        if(ni[i] < 5) {
-            return i;
-        }
-    }
-}
+
 
 
 
 const NormalDistCheck = ({a, sigma, intervals, ni}) => {
-    const fixNi = (ni, index) => {
-        
-        let tempH = intervals[index];
-        let tempN = ni[index];
 
-        intervals.splice(index, index);
-        ni.splice(index, index);
-        if(index !== 0) {
-            intervals[index-1][1] = tempH[1];
-            ni[index-1] += tempN;
+    // Create copies of the arrays
+    const newIntervals = [...intervals];
+    const newNi = [...ni];
+
+
+    const checkNi = (ni) => {
+        let toReturn;
+        for (let i = 0; i < ni.length; i++) {
+            if(ni[i] < 5) {
+                toReturn = i;
+            }
+            
         }
-        else {
-            intervals[index+1][0] = tempH[0];
-            ni[index+1] += tempN;
+        return toReturn;
+    }
+
+
+    const fixNi = (ni) => {
+        let index = checkNi(ni);
+        alert(index);
+        
+
+        
+
+        while(index !== undefined) {
+            let tempH = newIntervals[index];
+            let tempN = newNi[index];
+
+
+            
+
+            // Splice the copied arrays
+            newIntervals.splice(index, 1);
+            newNi.splice(index, 1);
+
+            
+
+            if(index !== 0) {
+                
+                newIntervals[index - 1][1] = tempH[1];
+
+                newNi[index - 1] += tempN;
+            }
+            else {
+
+                newIntervals[index][0] = tempH[0];
+
+                newNi[index] += tempN;
+            }
+
+            alert(`interval[1]: ${newIntervals[index][1]}`);
+            index = checkNi(newNi);
+
+            
+            alert(`index: ${index}`);
         }
+
+        
     }
     return ( 
         <div className='normal-dist-container'>
             <ShowTableContinious intervals={intervals} ni={ni} />
+            {fixNi(ni)}
+            <ShowTableContinious intervals={newIntervals} ni={newNi} />
+            
         </div>
      );
 }
